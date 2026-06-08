8d78f4e174174501c195a43fa132389f51d24a59
billy <billyjoelballola27@gmail.com>
2026-06-08 17:39:44 +0800

    fix: responsive design

--- src/components/dialog/CreateProjectDialog.tsx
+++ src/components/dialog/CreateProjectDialog.tsx
@@ -104,7 +104,7 @@ function CreateProjectDialog({ workspaceId }: { workspaceId: string }) {
           </div>
           <div className="space-y-2">
             <Label>Color</Label>
-            <div className="flex gap-2">
+            <div className="flex flex-wrap gap-2">
               {COLORS.map((color) => (
                 <button
                   key={color}
